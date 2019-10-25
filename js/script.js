8eabd5ed9ef088618c70056d0fc1026fe6c5bf68
skimanikings <kimanisimon856@gmail.com>
2019-10-25 15:23:11 +0300

    add variables to js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-/*var century,year,month,dayOfMonth
-var year = parseInt(document.getElementById("year").value);
-var month = parseInt(document.getElementById("month").value);
-var dayOfMonth = parseInt(document.getElementById("day").value);
-dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7);
-dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7)-1;
-return Math.floor(dayOfWeek);*/
 
+var daysOfTheWeek = ["Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday"]
+var MaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var FemaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var month = parseInt(document.getElementById("month").value)
+var year = parseInt(document.getElementById("year").value)
+var day = parseInt(document.getElementById("day").value)
+var century = parseInt(document.getElementById("century").value)
+var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
