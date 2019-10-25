cd35d8e86807910c33822e3ccd2d7de5c64f51fd
skimanikings <kimanisimon856@gmail.com>
2019-10-25 09:43:38 +0300

    add variables to javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert("welcome!");
+var century,year,month,dayOfMonth
+var year = parseInt(document.getElementById("year").value);
+var month = parseInt(document.getElementById("month").value);
+var dayOfMonth = parseInt(document.getElementById("day").value);
+dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7);
+dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7)-1;
+return Math.floor(dayOfWeek);
+
