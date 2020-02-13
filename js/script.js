7d48273948971c47e8aa694438d5ae4a22fe900e
skimanikings <kimanisimon856@gmail.com>
2020-02-13 12:51:25 +0300

    made changes on the js

--- js/script.js
+++ js/script.js
@@ -37,6 +37,17 @@ var submission = function() {
   var male = document.getElementById("male")
   var female = document.getElementById("female")
   
+  if (month =="" && year =="" && day =="" && name=="") {
+    alert("Please Enter you credentials");
+    return false;
+  }
+
+//   if (month == null || month== "", day == null || day == "", year== null || year == "", name == null || name == "") {
+//     alert("Please Fill All Required Field");
+//     return false;
+//   }
+// }
+  
   
  if (year < 0) {
     output.style.background ="red"
@@ -55,11 +66,11 @@ else if ((month < 1) || (month > 31)) {
     output.style.color= "white"
     output.innerHTML = "Hey! " + name + " please enter a valid day! "
 }
- var valid = (year>0 && (month > 0 && month <= 12) (day > 0 && day <32))
+//  var valid = (year>0 && (month > 0 && month <= 12) (day > 0 && day <32))
  
- if(valid == false){
-   alert("No Input received!!");
- }
+//  if(valid == false){
+//    alert("No Input received!!");
+//  }
 
   if(male.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
     output.style.background ="lime"
@@ -70,4 +81,5 @@ else if ((month < 1) || (month > 31)) {
     output.style.background ="red"
     output.innerHTML = "Hey! " + name + " you were born on a " + daysOfTheWeek[results] + " and your Akan name is " + femaleNames[results];
 }
+
 };
